b7d5a6b2dbe5221004e2c93afab905c579ea63bc
Anisa Tahlil <anisa.tahlil22@gmail.com>
2018-11-22 10:57:50 +0000

    final mongoose

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var mongoose=require("mongoose"),
     
     
     
-// mongoose.connect("mongodb://localhost/ssa_app", {useNewUrlParser:true});
-// mongoose.connect("mongodb://ssa:ssa2019@ds117729.mlab.com:17729/ssa" , {useNewUrlParser:true});
+
 mongoose.connect(process.env.DATABASEURL,{useNewUrlParser:true});
 app.set("view engine", "ejs");
 
